7520087b2f93c6f20cc2886d49d9aa1df6caef4a
moni2010 <85261089+moni2010@users.noreply.github.com>
2021-06-03 00:59:23 -0600

    Add files via upload

---
+++ Task2_5Except3and6.java
@@ -0,0 +1,10 @@
+public class Task2_5Except3and6 {
+    static public void main(String args[]){
+        int i=0;
+        while(i<6){
+            if(i!=3)
+                System.out.print(i+" ");
+            i++;
+        }
+    }
+}
